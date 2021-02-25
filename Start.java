0fabf1d37c1d22209a960f748d329696322fcfc7
lizf2000 <L15031234599@163.com>
2021-02-25 20:44:48 +0800

    first commit

---
+++ Start.java
@@ -0,0 +1,105 @@
+package Start01;
+
+public class Start {
+    public static void main (String[] args) {
+//    	       System.out.println("新的开始");
+//    	int a=100;
+//    	int sum=(1+a)*a/2;
+//    			System.out.println(sum);
+//    			char l='\n';
+//    			System.out.println(l);
+//    			double q=1.0;
+//    			double e=3.0;
+//    			double z=-4.0;
+//    			double w=Math.sqrt(e*e-4*q*z);
+//    			double r1=(-e+w)/2*q;
+//    			double r2=(-e-w)/2*q;
+//    			System.out.println(r1);
+//    			System.out.println(r2);
+//    			System.out.println(r1==1 && r2 == -4  ? "测试通过" : "测试失败");
+//    			System.out.println(l);
+//    			double A=3.6415926;
+//    			int B=(int)A;
+//    			System.out.println(B);
+//    			int C=(int) Math.round(A);
+//    			System.out.println(C);
+//    			System.out.println(l);
+//    			int i=123456789;
+//    			float o=i;
+//    			System.out.println(o);//简写强制转换
+//    			System.out.println(l);
+//    			System.out.println(5/2);//结果为整数
+//    			System.out.println(5%2);//结果为除后余数
+//    			System.out.println(5.0/2);//结果为小数
+//    			System.out.println(l);
+//    			int m=1;
+//    			m+=1;//简写++     ++表示+1
+//    			m+=2.5;
+//    			System.out.println(m);//结果为整数，自动四舍五入
+//    			System.out.println(l);
+//    			int age=7;
+//    			boolean isPrimarystudent=age>=6 && age<=12;
+//    			System.out.println(isPrimarystudent?"YES":"NO");////判断他是小学生嘛,如果是输出YES，如果不是输出NO
+//    			System.out.println(true && false && true);//&&（与）找最后一个假   
+//    			System.out.println(false && true && true);
+//  			System.out.println(true || false || true || false);//||（或）找第一个真
+//    			System.out.println(false || true && true);
+//    			System.out.println(true && true);
+//    			int c=6;
+//    			double d =(double) c+3.5;//强制转换为浮点型
+//    			System.out.println(d);
+//    			int q9=6;
+//    			boolean w6= q9!=6 ;
+//    			System.out.println(w6?"等于":"不等于");
+//    			System.out.println(l);
+//    			System.out.println(2+'A');//67
+//    			System.out.println(7+'中');//20020
+//    			System.out.println('\u0041');//A
+//    			System.out.println("NEW"+"\n");//输出NEW输出完毕后换行 
+//    			String a="字符";
+//    			String b="字符串";
+//    			String c=a+"\\"+b+"!";
+//    			System.out.println(c);//字符串连接（字符串连接“+”）
+//    			String ab="123\n"
+//    					+"456\n"
+//    					+"789";
+//    			System.out.println(ab);//输出多行字符串
+//    			String abc="new";
+//    		    String def="abc";
+//    			    	abc="day";
+//    			System.out.println(def);//字符串的内容不可变
+//              int a=72;
+//    			int b=105;
+//    			int c=65281;
+//    			String d=""+(char)a+(char)b+(char)c;
+//    			String v=""+(char)c;//字符串强制转换字符（""+(char)）
+//    			System.out.println(d);//字符串强制转换为字符，变成UTF——8编码。输出结果为：Hi！
+//    			int[]ns=new int[5];//数组：创建一个容纳5个int的数据类型.
+//    			ns[0]=68;          //数组的原始值都为0，布尔型为false。
+//    			ns[1]=79;          //数组一旦创建后，大小不能改变
+//    			ns[2]=91;//
+//    			ns[3]=85;
+//    			ns[4]=62;
+//    			System.out.println(ns[0]);
+//    			System.out.println(ns.length);//使用 数组变量名.length 可以查看创建了多少数组
+//    			int[] a= {1,3,5,7,9};//为int[] a=new int[]{1,3,5,7,9};的简写
+//    			System.out.println(a[1]);//输出创建的数组
+//    	        double a=3.1415926;
+//    			System.out.printf("%f.2",a);// %f为占位符表示格式化输出浮点数f后的数字代表输出小数点后多少位
+//    	        String a="Hello";
+//    	        String q=a.substring(1,5);//String类的substring方法可以从字符串中提取一个子符，提取位置；(起始,结束）注意：0为字符串的起点
+//    	        System.out.printf(q);
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    	        
+    }
+}
